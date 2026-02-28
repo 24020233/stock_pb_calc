e2b30b408725188dd594a0317dd3c7383c8117b8
wangzfox <358598720@qq.com>
2026-02-28 22:52:54 +0800

    代码清理

--- web/src/api/health.ts
+++
@@ -1,7 +0,0 @@
-import { http } from './http'
-import type { ApiResp } from './types'
-
-export async function apiHealth(): Promise<ApiResp<{ db: boolean; select: number | null }>> {
-  const resp = await http.get<ApiResp<{ db: boolean; select: number | null }>>('/api/health')
-  return resp.data
-}
